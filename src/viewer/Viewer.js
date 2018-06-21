65215cbaf7bd52a48c01b116a7a2929485ce31b2
Maxim Belsky <public.belsky@gmail.com>
2018-06-21 19:37:40 +0700

    Add Viewer stub

---
+++ src/viewer/Viewer.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class Viewer extends React.Component {
+  render() {
+    return (
+      <div>
+        <div>
+          <p>Current path</p>
+        </div>
+        <div>
+          <div>File #1</div>
+          <div>File #2</div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Viewer;
