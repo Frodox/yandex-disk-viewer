7c54f31d5653859d1a427576c526b7b0af0899fe
Maxim Belsky <public.belsky@gmail.com>
2018-06-22 13:33:57 +0700

    Load embedded items

--- src/viewer/Viewer.js
+++ src/viewer/Viewer.js
@@ -1,19 +1,58 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { loadFolder } from './actions';
 
 class Viewer extends React.Component {
-  render() {
+  _renderEmbeddedItems() {
+    const { items } = this.props;
+    if (!items) {
+      return null;
+    }
+
     return (
       <div>
-        <div>
-          <p>Current path</p>
-        </div>
-        <div>
-          <div>File #1</div>
-          <div>File #2</div>
-        </div>
+        {items.map(item => <div>{`${item.name} – ${item.size}`}</div>)}
       </div>
     );
   }
+
+  render() {
+    const { isLoading, name } = this.props;
+    let embeddedItems = this._renderEmbeddedItems();
+    let spinner = !isLoading ? null : <div>Spinner</div>;
+    return (
+      <div>
+        <div>
+          <p>{name}</p>
+        </div>
+        {embeddedItems}
+        {spinner}
+      </div>
+    );
+  }
+
+  componentDidMount() {
+    const { isLoading, onLoadFolder } = this.props;
+    if (!!isLoading) {
+      throw 'isLoading cannot be true here.';
+    }
+
+    onLoadFolder('/');
+  }
 }
 
-export default Viewer;
+const mapStateToProps = state => {
+  return state.folder;
+};
+const mapDispatchToProps = dispatch => {
+  return {
+    onLoadFolder: path => {
+      dispatch(loadFolder(path));
+    },
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Viewer);
