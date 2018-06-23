56ff159fb860748d1e6324343490b2e98a9b4f46
Maxim Belsky <public.belsky@gmail.com>
2018-06-23 12:09:38 +0700

    Implement breadcrumb

--- src/breadcrumb/Breadcrumb.js
+++ src/breadcrumb/Breadcrumb.js
@@ -1,18 +1,44 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './breadcrumb.css';
 
 class Breadcrumb extends React.Component {
+  _renderItem(item, index, items) {
+    const isLast = index === items.length - 1;
+    return (
+      <li
+        className={'breadcrumb-item ' + (isLast ? '' : 'active')}
+        key={item.path}
+      >
+        {isLast ? item.name : <Link to={item.path}>{item.name}</Link>}
+      </li>
+    );
+  }
+
   _renderItems() {
-    const { pathname } = this.props;
-    const items = ['Home'].concat(pathname.split('/').filter(item => 0 !== item.length));
-    //TODO: Change text with Links
-    return items.map((item, index) => <li class={'breadcrumb-item ' + (index === items.length - 1 ? 'active' : '')}>{item}</li>);
+    let { pathname } = this.props;
+    const items = [];
+
+    while (0 !== pathname.length) {
+      const index = pathname.lastIndexOf('/');
+      const lastPath = pathname.substring(index + 1);
+
+      if (0 !== lastPath.length) {
+        items.unshift({ name: lastPath, path: pathname });
+      }
+
+      pathname = pathname.substring(0, index);
+    }
+
+    items.unshift({ name: 'Home', path: '/' });
+
+    return items.map(this._renderItem);
   }
 
   render() {
     return (
-      <nav role="navigation">
-        <ol class="breadcrumb breadcrumb_no-margin">
+      <nav>
+        <ol className="breadcrumb breadcrumb_no-margin">
           {this._renderItems()}
         </ol>
       </nav>
